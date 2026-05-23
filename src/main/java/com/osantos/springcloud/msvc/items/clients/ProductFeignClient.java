edcd9380ebcfbaf50ed6ca2cf4bedeb956ee6d97
Oswaldo <santoswaldos@gmail.com>
2026-05-23 17:54:29 -0600

    Clase para consumir el API REST

---
+++ src/main/java/com/osantos/springcloud/msvc/items/clients/ProductFeignClient.java
@@ -0,0 +1,23 @@
+package com.osantos.springcloud.msvc.items.clients;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.osantos.springcloud.msvc.items.models.Product;
+
+//Clase para consumir el API REST
+@FeignClient(url = "localhost:8001") // Se especifica el puerto del proyecto Products
+public interface ProductFeignClient {
+
+    @GetMapping
+    List<Product> findAll();
+
+    // Tiene que se el mismo nombre de rutas para poder acceder y los mismo
+    // parámetros y el mismo tipo de retorno
+    @GetMapping("/{id}")
+    Product details(@PathVariable Long id);
+
+}
