fd38846fefc24eac462f9bbfcec9451cdc08a568
Oswaldo <santoswaldos@gmail.com>
2026-05-23 21:11:34 -0600

    Ajustando para atrapar la exception FeignException y poder tratarla para mandar un error 404 con un mensaje

--- src/main/java/com/osantos/springcloud/msvc/items/services/ItemServiceFeign.java
+++ src/main/java/com/osantos/springcloud/msvc/items/services/ItemServiceFeign.java
@@ -12,6 +12,8 @@ import com.osantos.springcloud.msvc.items.clients.ProductFeignClient;
 import com.osantos.springcloud.msvc.items.models.Item;
 import com.osantos.springcloud.msvc.items.models.Product;
 
+import feign.FeignException;
+
 @Service
 public class ItemServiceFeign implements ItemService {
 
@@ -42,13 +44,15 @@ public class ItemServiceFeign implements ItemService {
 
     @Override
     public Optional<Item> findById(Long id) {
-        Product product = client.details(id);
-        if (product == null) {
+
+        try {
+            Product product = client.details(id);
+            return Optional.of(
+                    new Item(client.details(id), new Random().nextInt(10) + 1));
+
+        } catch (FeignException e) {
             return Optional.empty(); // Regresa un 404
         }
-
-        return Optional.of(
-                new Item(client.details(id), new Random().nextInt(10) + 1));
 
     }
 
